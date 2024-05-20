373b2839b088bdce9146025e8cd2bc1226a3b368
docsdeveloperdemo <151708530+docsdeveloperdemo@users.noreply.github.com>
2024-05-20 16:34:55 -0700

    Update SwiftStarterAppTests.swift

--- SwiftStarterAppTests/SwiftStarterAppTests.swift
+++ SwiftStarterAppTests/SwiftStarterAppTests.swift
@@ -11,7 +11,7 @@ import XCTest
 final class SwiftStarterAppTests: XCTestCase {
 
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        // Put setup code here. This method is called before the invocation of each test method in the class. set it up
     }
 
     override func tearDownWithError() throws {
