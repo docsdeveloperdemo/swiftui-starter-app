cdb10f9b15cddd213982ae38b636c0127cdffefb
docsdeveloperdemo <151708530+docsdeveloperdemo@users.noreply.github.com>
2024-05-20 22:36:07 -0700

    Update SwiftStarterApp.swift

--- SwiftStarterApp/SwiftStarterApp.swift
+++ SwiftStarterApp/SwiftStarterApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 @main
 struct SwiftStarterApp: App {
   let persistenceController = PersistenceController.shared.viewContext
-    //very neat
+    //very neato
     var body: some Scene {
         WindowGroup {
           NavigationSplitHomeView()
